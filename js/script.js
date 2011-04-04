56803be89dd17f5b3e646edfbf6c2cf6c770e7c9
Dan Harper <intouch@danharper.me>
2011-04-04 20:32:52 +0100

    mail boxes slide up

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ $(function() {
 	var to = $('#to');
 	var message = $('#composemail .message textarea');
 
-	// $(toggleTo).hide();
-	// $(toggleMessage).hide();
-	$('#composemail .compose').hide(); // swap round later
+	$(toggleTo).hide();
+	$(toggleMessage).hide();
+	// $('#composemail .compose').hide(); // swap round later
 
 	$(compose).focus(function() {
 		$(toggleCompose).hide();
@@ -33,16 +33,40 @@ $(function() {
 		$(to).focus();
 	});
 
+	// Add name to list
 	$(to).keypress(function(e) {
 		if (e.which === 13) {
-			$('<span>' + to.val() + ' <b>x</b></span>').appendTo('#composemail .recipients div');
-			$(to).val('');
+			if ($(to).val().length > 0) {
+				$('<span>' + to.val() + ' <b>x</b></span>').appendTo('#composemail .recipients div');
+				$(to).val('');
+			}
 			return false;
+		}
+	});
+
+	// Remove last name from list on backspace, if textbox is empty
+	$(to).keydown(function(e) {
+		if (e.which === 8) {
+			input = $(to).val();
+			recipients = $('#composemail .recipients span');
+			if (input.length === 0 && recipients.length > 0) {
+				$(recipients).last().remove();
+			}
 		}
 	});
 
 	$('#composemail .recipients span b').live("click", function() {
 		$(this).parent().remove();
+	});
+
+	// Nothing in the boxes? Close it.
+	$(message).blur(function() {
+		recipients = $('#composemail .recipients span');
+		if ($(to).val().length === 0 && recipients.length === 0 && $(message).val().length === 0) {
+			$(toggleTo).hide();
+			$(toggleCompose).show();
+			$(toggleMessage).slideUp();
+		}
 	});
 
 });
