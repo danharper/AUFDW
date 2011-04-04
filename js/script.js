d57b35eb64456e798a8b6dbd5b9006370d72ccab
Dan Harper <intouch@danharper.me>
2011-04-04 15:01:25 +0100

    notifications

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 $(function() {
 
+	var compose = $('#composemail .compose input');
+	var toggleCompose = $('#composemail .compose');
+	var toggleTo = $('#composemail .to');
+	var toggleMessage = $('#composemail .message');
+
 	var to = $('#composemail .to input');
 	var message = $('#composemail .message textarea');
+
+	$(toggleTo).hide();
+	$(toggleMessage).hide();
+
+	$(compose).focus(function() {
+		$(toggleCompose).hide();
+		$(toggleTo).show();
+		$(to).focus();
+		$(toggleMessage).slideDown();
+	});
 
 	$(to).focus(function() {
 		$(message).addClass('highlightTop');
