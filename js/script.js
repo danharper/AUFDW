ad9830c3f3b0d0fe0bbf4acd76674dcc4ce8eb36
Dan Harper <intouch@danharper.me>
2011-04-04 13:35:39 +0100

    working

--- js/script.js
+++ js/script.js
@@ -1,26 +1,14 @@
 $(function() {
 
-  // var composemail = $('#composemail input[type=text], #composemail textarea');
+	var to = $('#composemail input.to');
+	var message = $('#composemail textarea.message');
 
-  // $(composemail).focus(function() {
-    // $(this).addClass('focus');
-  // });
+	$(to).focus(function() {
+		$(message).addClass('highlightTop');
+	});
 
-  // $(composemail).blur(function() {
-    // $(this).removeClass('focus');
-  // });
-
-  var to = $('#composemail label[for=to], #composemail input[name=to]');
-  var message = $('#composemail label[for=message], #composemail textarea');
-
-  $(to).focus(function() {
-    $(this).addClass('addBorder');
-    $(message).addClass('removeBorder');
-  });
-
-  $(to).blur(function() {
-    $(this).removeClass('addBorder');
-    $(message).removeClass('removeBorder');
-  });
+	$(to).blur(function() {
+		$(message).removeClass('highlightTop');
+	});
 
 });
